a10361509a7a3e2fae0d3fa92c3b99bdd56705bc
NikolaAgbaba <nikola.agbaba@gmail.com>
2023-11-03 11:43:25 +0100

    Renamed the helper class so that it makes more sense, deleted commented code

--- src/test/java/Helper.java
+++ src/test/java/Helper.java
@@ -1,23 +1,20 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.Driver;
-import java.time.Duration;
 import java.util.List;
 
-public class Test1 {
+public class Helper {
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public Test1(){
+    public Helper() {
 
     }
 
-    public Test1(WebDriver driver, WebDriverWait wait){
+    public Helper(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
     }
@@ -39,7 +36,7 @@ public class Test1 {
     }
 
     //method for login
-    public void login(String username, String password){
+    public void login(String username, String password) {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("orangehrm-login-button")));
@@ -52,8 +49,8 @@ public class Test1 {
     }
 
     //method for checking if the url ends with a valid path
-        public boolean isUrlValid(String urlEnd){
-            return driver.getCurrentUrl().endsWith(urlEnd);
+    public boolean isUrlValid(String urlEnd) {
+        return driver.getCurrentUrl().endsWith(urlEnd);
     }
 
     //method for navigation through the pages
@@ -69,27 +66,14 @@ public class Test1 {
         }
     }
 
-//        public void getNavElementName(){
-//            login("Admin", "admin123");
-//            try {
-//                Thread.sleep(3000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//            List<WebElement> navigationElements = driver.findElements(By.className("oxd-main-menu-item"));
-//            for (WebElement el : navigationElements) {
-//                System.out.println(el.getText());
-//            }
-//        }
-
     //getting the error message (invalid credentials)
-    public String getLoginErrorMessage(){
+    public String getLoginErrorMessage() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("oxd-alert--error")));
         return driver.findElement(By.className("oxd-alert--error")).getText();
     }
 
     //getting the empty field error message
-    public String getEmptyFieldErrorMessage(){
+    public String getEmptyFieldErrorMessage() {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("oxd-input-field-error-message")));
         return driver.findElement(By.className("oxd-input-field-error-message")).getText();
     }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,184 +1,158 @@
 import dev.failsafe.internal.util.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.annotation.Nullable;
 import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        Test1 test1 = new Test1(driver, wait);
+        Helper helper = new Helper(driver, wait);
 
         //login with valid credentials
-        test1.login("Admin", "admin123");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/dashboard/index"), "Url is not valid, the user isn't able to login");
+        helper.login("Admin", "admin123");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/dashboard/index"), "Url is not valid, the user isn't able to login");
         driver.quit();
 
         //login using invalid username and a valid password
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.login("Admi", "admin123");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid username");
+        helper = new Helper(driver, wait);
+        helper.login("Admi", "admin123");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid username");
         String expectedMessage = "Invalid credentials";
-        Assert.isTrue(expectedMessage.equals(test1.getLoginErrorMessage()), "Unsuccessful login message isn't valid");
+        Assert.isTrue(expectedMessage.equals(helper.getLoginErrorMessage()), "Unsuccessful login message isn't valid");
         driver.quit();
 
         //login using a valid username and invalid password
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.login("Admin", "admin12");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid password");
-        Assert.isTrue(expectedMessage.equals(test1.getLoginErrorMessage()), "Unsuccessful login message isn't valid");
+        helper = new Helper(driver, wait);
+        helper.login("Admin", "admin12");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid password");
+        Assert.isTrue(expectedMessage.equals(helper.getLoginErrorMessage()), "Unsuccessful login message isn't valid");
         driver.quit();
 
         //login using a valid username and leaving the password field empty
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.login("Admin", "");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid password");
+        helper = new Helper(driver, wait);
+        helper.login("Admin", "");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid password");
         String expectedRequiredFieldMessage = "Required";
-        Assert.isTrue(expectedRequiredFieldMessage.equals(test1.getEmptyFieldErrorMessage()), "Empty field message isn't displayed");
+        Assert.isTrue(expectedRequiredFieldMessage.equals(helper.getEmptyFieldErrorMessage()), "Empty field message isn't displayed");
         driver.quit();
+
 
         //login using a valid password and leaving the username field empty
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.login("", "admin123");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid password");
-        Assert.isTrue(expectedRequiredFieldMessage.equals(test1.getEmptyFieldErrorMessage()), "Empty field message isn't displayed");
+        helper = new Helper(driver, wait);
+        helper.login("", "admin123");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/auth/login"), "Url is not valid, the user is able to login using invalid password");
+        Assert.isTrue(expectedRequiredFieldMessage.equals(helper.getEmptyFieldErrorMessage()), "Empty field message isn't displayed");
         driver.quit();
+
 
         //going to the Admin page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Admin");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/admin/viewSystemUsers"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Admin");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/admin/viewSystemUsers"), "The user is not on the valid page");
         driver.quit();
+
 
         //going to the PIM page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("PIM");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/pim/viewEmployeeList"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("PIM");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/pim/viewEmployeeList"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Leave page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Leave");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/leave/viewLeaveList"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Leave");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/leave/viewLeaveList"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Time page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Time");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/time/viewEmployeeTimesheet"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Time");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/time/viewEmployeeTimesheet"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Recruitment page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Recruitment");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/recruitment/viewCandidates"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Recruitment");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/recruitment/viewCandidates"), "The user is not on the valid page");
         driver.quit();
 
         //going to the My Info page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("My Info");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/pim/viewPersonalDetails/empNumber/7"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("My Info");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/pim/viewPersonalDetails/empNumber/7"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Performance page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Performance");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/performance/searchEvaluatePerformanceReview"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Performance");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/performance/searchEvaluatePerformanceReview"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Dashboard page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Dashboard");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/dashboard/index"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Dashboard");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/dashboard/index"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Directory page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Directory");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/directory/viewDirectory"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Directory");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/directory/viewDirectory"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Maintenance page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Maintenance");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/maintenance/purgeEmployee"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Maintenance");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/maintenance/purgeEmployee"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Claim page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Claim");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/claim/viewAssignClaim"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Claim");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/claim/viewAssignClaim"), "The user is not on the valid page");
         driver.quit();
 
         //going to the Buzz page
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        test1 = new Test1(driver, wait);
-        test1.goToThePage("Buzz");
-        Assert.isTrue(test1.isUrlValid("/web/index.php/buzz/viewBuzz"), "The user is not on the valid page");
+        helper = new Helper(driver, wait);
+        helper.goToThePage("Buzz");
+        Assert.isTrue(helper.isUrlValid("/web/index.php/buzz/viewBuzz"), "The user is not on the valid page");
         driver.quit();
 
 
-
-
-        //valid login test
-//        login("Admin", "admin123");
-//        Assert.isTrue(isUrlValid("/web/index.php/dashboard/index"), "Url is not valid");
     }
-
-
-//    public static void login(String username, String password){
-//        WebDriver driver = new ChromeDriver();
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-//        driver.manage().window().maximize();
-//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("orangehrm-login-button")));
-//        WebElement usernameField = driver.findElement(By.name("username"));
-//        WebElement passwordField = driver.findElement(By.name("password"));
-//        WebElement loginButton = driver.findElement(By.className("orangehrm-login-button"));
-//        usernameField.sendKeys(username);
-//        passwordField.sendKeys(password);
-//        loginButton.click();
-//        driver.quit();
-//    }
-
-//    public static boolean isUrlValid(String urlEnd){
-//        return getCurrentUrl().endsWith(urlEnd);
-//    }
 }
