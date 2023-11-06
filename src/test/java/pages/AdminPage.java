474ad6261e042e5033a6c954328d049421fc473c
NikolaAgbaba <nikola.agbaba@gmail.com>
2023-11-06 11:44:35 +0100

    Added admin page and admin page tests in order to see if the admin can add, edit and delete jobs

--- src/test/java/pages/AdminPage.java
+++ src/test/java/pages/AdminPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -29,9 +28,52 @@ public class AdminPage extends BasePage{
     @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[2]/a")
     private WebElement payGradesButton;
 
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[3]/a")
+    private WebElement employmentStatusButton;
 
-    //method for going to the job titles
-    public void goToTheJobTitles(String navigationButtonName, String dropdownElementName){
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[4]/a")
+    private WebElement jobCategoriesButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul/li[5]/a")
+    private WebElement workShiftsButton;
+
+    @FindBy(className = "orangehrm-container")
+    private WebElement jobsContainer;
+
+    @FindBy(className = "oxd-table-card")
+    private List<WebElement> jobsList;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div[3]/div/div[2]/div/div/div[2]")
+    private List<WebElement> jobTitlesList;
+
+    @FindBy(className = "oxd-button-icon")
+    private WebElement addJobButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input")
+    private WebElement jobTitleInputField;
+
+    @FindBy(className = "orangehrm-left-space")
+    private WebElement saveButton;
+
+    @FindBy(xpath = "//*[@id=\"oxd-toaster_1\"]/div/div[1]/div[2]/p[1]")
+    private WebElement savedMessage;
+
+    @FindBy(className = "bi-trash")
+    private WebElement deleteJobButton;
+
+    @FindBy(className = "bi-pencil-fill")
+    private WebElement editJobButton;
+
+    @FindBy(className = "oxd-button--label-danger")
+    private WebElement deleteConfirmationButton;
+
+    //getter for job titles list
+    public WebElement getJobsContainer() {
+        return jobsContainer;
+    }
+
+    //method for navigating through the job dropdown menu
+    public void jobDropdownNavigation(String navigationButtonName, String dropdownElementName){
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("oxd-topbar-body-nav")));
         for (WebElement el: topNavButtonsList){
             if (el.getText().equals(navigationButtonName)){
@@ -43,14 +85,74 @@ public class AdminPage extends BasePage{
                         break;
                     case "Pay Grades":
                         payGradesButton.click();
+                        break;
+                    case "Employment Status":
+                        employmentStatusButton.click();
+                        break;
+                    case "Job Categories":
+                        jobCategoriesButton.click();
+                        break;
+                    case "Work Shifts":
+                        workShiftsButton.click();
+                        break;
                 }
                 break;
             }
         }
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+    }
+
+    //method for adding a job title
+    public void addAJobTitle(String jobTitle){
+        addJobButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input")));
+        jobTitleInputField.sendKeys(jobTitle);
+        saveButton.click();
+    }
+
+//    public String getJobTitleInputFieldValue(){
+//        return jobTitleInputField.getAttribute("value");
+//    }
+
+    //method for checking if the job is present in the jobs list
+    public boolean isJobPresent(String jobTitle){
+        boolean jobTitleIsPresent = false;
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("orangehrm-container")));
+        for (WebElement el: jobTitlesList){
+            if (el.getText().equals(jobTitle)){
+                jobTitleIsPresent = true;
+                break;
+            }
         }
+        return jobTitleIsPresent;
+    }
+
+    //method for deleting the job
+    public void deleteTheJob(String jobName){
+        for (WebElement el: jobsList){
+            if (el.getText().contains(jobName)){
+                el.findElement(By.className("bi-trash")).click();
+                break;
+            }
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("oxd-button--label-danger")));
+        deleteConfirmationButton.click();
+    }
+
+    //method for editing the job
+    public void editTheJob(String jobName){
+        for (WebElement el: jobsList){
+            if (el.getText().contains(jobName)){
+                el.findElement(By.className("bi-pencil-fill")).click();
+                break;
+            }
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input")));
+        jobTitleInputField.sendKeys(" - edited job");
+        saveButton.click();
+    }
+
+    //getting the message text
+    public String getMessageText(){
+        return savedMessage.getText();
     }
 }
--- src/test/java/tests/AdminPageTests.java
+++ src/test/java/tests/AdminPageTests.java
@@ -1,8 +1,10 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.AdminPage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -11,6 +13,8 @@ public class AdminPageTests extends BaseTest{
     private AdminPage adminPage;
     private LoginPage loginPage;
     private HomePage homePage;
+    private String jobTitle;
+    private String editedJob;
 
     @BeforeClass
     public void beforeClass(){
@@ -18,6 +22,8 @@ public class AdminPageTests extends BaseTest{
         adminPage = new AdminPage(driver, wait, faker);
         loginPage = new LoginPage(driver, wait, faker);
         homePage = new HomePage(driver, wait, faker);
+        jobTitle = faker.job().title();
+        editedJob =  jobTitle + " - edited job";
     }
 
     @BeforeMethod
@@ -27,8 +33,45 @@ public class AdminPageTests extends BaseTest{
         homePage.goToThePage("Admin");
     }
 
-    @Test
-    public void goToTheJobTitles(){
-        adminPage.goToTheJobTitles("Job", "Job Titles");
+    //testing if the jobs container is displayed
+    @Test (priority = 1)
+    public void areJobTitlesDisplayed(){
+        adminPage.jobDropdownNavigation("Job", "Job Titles");
+        Assert.assertTrue(adminPage.getJobsContainer().isDisplayed());
+    }
+
+    //testing if the newly added job is present in the jobs list
+    @Test (priority = 2)
+    public void addANewJobTitle(){
+        SoftAssert softAssert = new SoftAssert();
+        String expectedMessage = "Success";
+        adminPage.jobDropdownNavigation("Job", "Job Titles");
+        adminPage.addAJobTitle(jobTitle);
+        softAssert.assertEquals(adminPage.getMessageText(), expectedMessage);
+        softAssert.assertTrue(adminPage.isJobPresent(jobTitle));
+        softAssert.assertAll();
+    }
+
+    //testing if the job can be deleted
+    @Test (priority = 4)
+    public void deleteTheJob(){
+        SoftAssert softAssert = new SoftAssert();
+        String expectedMessage = "Success";
+        adminPage.jobDropdownNavigation("Job", "Job Titles");
+        adminPage.deleteTheJob(editedJob);
+        softAssert.assertEquals(adminPage.getMessageText(), expectedMessage);
+        softAssert.assertTrue(!adminPage.isJobPresent(editedJob));
+        softAssert.assertAll();
+    }
+
+    //testing if the job can be edited
+    @Test (priority = 3)
+    public void editTheJob(){
+        SoftAssert softAssert = new SoftAssert();
+        String expectedMessage = "Success";
+        adminPage.jobDropdownNavigation("Job", "Job Titles");
+        adminPage.editTheJob(jobTitle);
+        softAssert.assertEquals(adminPage.getMessageText(), expectedMessage);
+        softAssert.assertTrue(adminPage.isJobPresent(editedJob));
     }
 }
